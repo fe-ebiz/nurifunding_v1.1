68222362c9f1ff6c80745b092704cc17d670f71d
vxggangxv <vxggangxv@naver.com>
2020-02-11 17:51:53 +0900

    edit main_v2

---
+++ server/service/ceo.php
@@ -0,0 +1,146 @@
+<?php
+	$page_type = "service";
+	include_once("/home/ebizdev/web-home/nurifunding.co.kr/www/web-home/common/top.php");
+?>
+		<section class="page-sect">
+			<div class="container">
+				<div class="ps-wrap com-box">
+					<!--변경내용 제목변경-->
+					<h2 class="ps-h2">대표이사 소개</h2>
+				</div>
+			</div>
+		</section>
+
+		<section class="info-sect rep-sect">
+			<div class="container">
+
+				<!--변경내용 시작 전체변경-->
+				<div class="is-wrap com-box low">
+					
+					<div class="is-box nth-5 type-2 col-xs-12 col-sm-12">
+						<div class="d2">
+							<h3 class="is-h3">누리펀딩 <span class="c-blue">대표이사 소개</span></h3>
+							<p class="p1-1">
+								김정권 (1963년생) <br>
+								여신심사협의회 의장
+							</p>
+							
+							
+
+							<ul class="history nth-1">
+								<li class="clearfix"><div>주요 이력 (KB 국민은행 30년 근무)</div></li>
+								<li class="clearfix">
+									<div class="c-blue pull-left">2008</div>
+									<div class="pull-right">헬싱키 경제경영대학원 MBA졸업</div>
+								</li>
+								<li class="clearfix">
+									<div class="c-blue pull-left">1999</div>
+									<div class="pull-right">국민은행 여신심사역 과정</div>
+								</li>
+								<li class="clearfix">
+									<div class="c-blue pull-left">2000</div>
+									<div class="pull-right c-blue f-bd">국민은행 심사센터 여신심사역 (전업종)</div>
+								</li>
+								<li class="clearfix">
+									<div class="c-blue pull-left">2002</div>
+									<div class="pull-right">마포기업금융지점 팀장 (RM/PF/대출)</div>
+								</li>
+								<li class="clearfix">
+									<div class="c-blue pull-left">2007</div>
+									<div class="pull-right c-blue f-bd">국민은행 기업여신심사부 팀장 <br class="show-xs">(투자금융/SOC/유동화/해외투자)</div>
+								</li>
+								<li class="clearfix">
+									<div class="c-blue pull-left">2010</div>
+									<div class="pull-right">국민은행 지점장 (망원동/시화공단/목동)</div>
+								</li>
+								<li class="clearfix">
+									<div class="c-blue pull-left">2014</div>
+									<div class="pull-right">국민은행 기관영업부 부장 <br class="show-xs">(정부기관/시도금고/학교/병원 협약)</div>
+								</li>
+								<li class="clearfix">
+									<div class="c-blue pull-left">2016</div>
+									<div class="pull-right c-blue f-bd">국민은행 지역본부장 (중앙2 지역, 인천북2 지역)</div>
+								</li>
+								<li class="clearfix">
+									<div class="c-blue pull-left">2018</div>
+									<div class="pull-right">(주)누리펀딩 창업</div>
+								</li>
+							</ul>
+							
+							<ul class="history nth-1">
+								<li class="clearfix">
+									<div>수상이력</div>
+								</li>
+								<li class="clearfix">
+									<div class="c-blue pull-left">2005</div>
+									<div class="pull-right">국민은행 국은인상</div>
+								</li>
+								<li class="clearfix">
+									<div class="c-blue pull-left">2014</div>
+									<div class="pull-right">국방부장관 표창장</div>
+								</li>
+							</ul>
+						</div>
+						<div class="d1">
+							<img class="img-rep" src="https://nurifunding.co.kr/img/service/photo_rep.png" alt="대표사진">
+						</div>
+					</div>
+					
+				</div>
+				<!--변경내용 끝-->
+				
+				<!-- 180731 이사 추가 -->
+				<!-- <div class="is-box nth-7 type-2 col-xs-12 col-sm-12 clearfix">
+					<div class="rep-area">
+						<div class="rep-img"><img src="https://nurifunding.co.kr/img/service/img_rep02.jpg" alt="이사 신상천"></div>
+						<div class="history-box">
+							<div class="d2">
+								<h3 class="is-h3">이사 신상천</h3>
+
+								<p class="history-tit">건국대 경영대학원(MBA) 졸업</p>
+								<ul class="history-list">
+									<li>
+										<div class="fir c-blue">2004</div>
+										<div class="">국민은행 기업여신심사부 심사역 </div>
+									</li>
+									<li>
+										<div class="fir c-blue">2012</div>
+										<div class="">KB 금융지주 대외협력 Unit장 </div>
+									</li>
+									<li>
+										<div class="fir c-blue">2015</div>
+										<div class="">국민은행 지점장<br>(잠실나루역, 신월뉴타운)</div>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div class="rep-area">
+						<div class="rep-img"><img src="https://nurifunding.co.kr/img/service/img_rep03.jpg" alt="이사 강병남"></div>
+						<div class="history-box">
+							<div class="d2">
+								<h3 class="is-h3">이사 강병남</h3>
+
+								<p class="history-tit">경희대학교 경제학과 졸업</p>
+								<ul class="history-list">
+									<li>
+										<div class="fir c-blue">2004</div>
+										<div class="">가계여신부, 소호 심사 규정 담당 </div>
+									</li>
+									<li>
+										<div class="fir c-blue">2014</div>
+										<div class="">국민은행 지점장<br>(쌍촌동, 삼선교, 의정부)</div>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div> -->
+
+			</div>
+		</section>
+
+	</div>
+<?php
+	include_once("/home/ebizdev/web-home/nurifunding.co.kr/www/web-home/common/bottom.php");
+?>
